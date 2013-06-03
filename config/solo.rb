d193bb10eb21f15de03ea7d1f22c8b3e37b8f87b
Ben Cordero (bencord0) <bmc@linux.com>
2013-06-03 20:30:58 +0000

    Log to stdout

--- config/solo.rb
+++ config/solo.rb
@@ -3,3 +3,6 @@ root = File.absolute_path(File.dirname(__FILE__))
 cookbook_path   [ root + '../cookbooks' ]
 data_bag_path   root + '../data_bags'
 role_path       root + '../roles'
+
+log_level       :info
+log_location    STDOUT
