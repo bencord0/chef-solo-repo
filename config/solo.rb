85073541b2fe7fa8dad9a3137df4ba88fb98c357
Ben Cordero <bmc@linux.com>
2013-06-27 15:50:06 +0100

    use string templating

--- config/solo.rb
+++ config/solo.rb
@@ -1,8 +1,8 @@
 root = File.absolute_path(File.dirname(__FILE__))
 
-cookbook_path   [ root + '../cookbooks' ]
-data_bag_path   root + '../data_bags'
-role_path       root + '../roles'
+cookbook_path   [ "#{root}/../cookbooks" ]
+data_bag_path   "#{root}/../data_bags"
+role_path       "#{root}/../roles"
 
 log_level       :info
 log_location    STDOUT
